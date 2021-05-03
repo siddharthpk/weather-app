ad86a6ff5ccec60611efd1c52b2933a65e0449ab
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-03 02:26:23 -0700

    minor updates

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,8 +3,8 @@ const request = require('request')
 
 // Add forecast feature here
 const forecast = (lat,long, callback) =>{
-    const url = 'http://api.weatherstack.com/current?access_key=96a6319277839602fd243c69140c9f03&query=37.8267,-122.4233' + encodeURIComponent(lat) +encodeURIComponent(long)+ '&units=f'
-
+    const url = 'http://api.weatherstack.com/current?access_key=96a6319277839602fd243c69140c9f03&query=' + lat +','+long+ '&units=f'
+    console.log(url)
     request({
         url: url,
         json: true
@@ -12,8 +12,8 @@ const forecast = (lat,long, callback) =>{
         if(error){
             callback('Unable to connect to weather services', undefined)
         }
-        else if(response.body.current.error){
-            callback('Unable to find weather for location. Try another search', undefined)
+        else if(response.body.error){
+            callback('Unable to find weather for location.', undefined)
         }
         else{
             callback(undefined, {
