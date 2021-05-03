fb00753e2ed8a7dffc8c098131c1231599cac70f
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-02 21:58:16 -0700

    Added api end error handling

--- app.js
+++ app.js
@@ -12,8 +12,17 @@ request({
     //console.log(response)
     // const data = JSON.parse(response.body)
     // console.log(data.current)
+    if(error){
+        console.log('Unable to connect to weather service')
+    }
 
-    console.log(response.body.current)
+    else if(response.body.error){
+        console.log('Unable to find location')
+    }
+    else{
+        console.log(response.body.current)
+    }
+    
 })
 
 
@@ -22,5 +31,17 @@ request({
     url: url,
     json: true
 }, (error, response) =>{
-    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature+" degrees out. It feels like "+ response.body.current.feelslike + " degrees out.")
+
+    //Add Error Handling here
+    if(error){
+        console.log('Unable to connect to weather service')
+    }
+
+    else if(response.body.error){
+        console.log('Unable to find location')
+    }
+    else{
+        console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature+" degrees out. It feels like "+ response.body.current.feelslike + " degrees out.")
+    }
+    
 })
