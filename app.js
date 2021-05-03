e10887e53739631ed042f0c2a09868b696f0f589
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-02 19:59:11 -0700

    Added current weather request from API

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ const request = require('request')
 //Store API url
 const url = 'http://api.weatherstack.com/current?access_key=96a6319277839602fd243c69140c9f03&query=37.8267,-122.4233'
 
-//Requesting API response using request 
+//Requesting API response using request package
 request({
-    url: url
+    url: url,
+    json: true
 }, (error, response)=>{
     //console.log(response)
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+    // const data = JSON.parse(response.body)
+    // console.log(data.current)
+
+    console.log(response.body.current)
+})
+
+
+//Requesting Current weather data from API
+request({
+    url: url,
+    json: true
+}, (error, response) =>{
+    console.log("It is currently " + response.body.current.temperature+" degrees out. It feels like "+ response.body.current.feelslike + " degrees out.")
 })
