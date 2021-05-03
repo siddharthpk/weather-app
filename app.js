00bbf8cb321cba94ae86f7ba3909575b51940e85
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-03 02:39:13 -0700

    Added callback chaining to geocode & forecast

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-// Add require "request" package
-const request = require('request')
-
 //Importing geocode.js & forecast.js from utils
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-//Call geocode
-geocode('Vancouver', (error,data)=>{
-    console.log('Error', error)
-    console.log('Data', data)
+//Call geocode & forecast using callback chaining
+geocode('Boston', (error,data)=>{
+    if(error){
+    }
+    console.log(data)
+    forecast(data.latitude,data.longitude, (error, forecastData) => {
+        if(error){
+           return console.log(error)
+        }
+        console.log(data.location)
+        console.log(forecastData)
+    })
 })
-
-//Call forecast
-forecast(37.8267,-122.4233, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
