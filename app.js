290992451d853b88a6416310b9b89a33863283f7
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-02 19:36:16 -0700

    Accessing weather api using request package

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+// Add require "request" package
 const request = require('request')
+
+//Store API url
+const url = 'http://api.weatherstack.com/current?access_key=96a6319277839602fd243c69140c9f03&query=37.8267,-122.4233'
+
+//Requesting API response using request 
+request({
+    url: url
+}, (error, response)=>{
+    console.log(response)
+})
