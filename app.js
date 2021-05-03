6d475436335175bb82541200272aefa482340232
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-03 01:52:40 -0700

    Added geocode import

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
 // Add require "request" package
 const request = require('request')
 
+//Importing geocode.js from utils
+const geocode = require('./utils/geocode')
+
 //Store API url
 const url = 'http://api.weatherstack.com/current?access_key=96a6319277839602fd243c69140c9f03&query=37.8267,-122.4233&units=f'
 
-//Requesting API response using request package & HTTP requests
-request({
-    url: url,
-    json: true
-}, (error, response)=>{
-    //console.log(response)
-    // const data = JSON.parse(response.body)
-    // console.log(data.current)
-    if(error){
-        console.log('Unable to connect to weather service')
-    }
+// //Requesting API response using request package & HTTP requests
+// request({
+//     url: url,
+//     json: true
+// }, (error, response)=>{
+//     //console.log(response)
+//     // const data = JSON.parse(response.body)
+//     // console.log(data.current)
+//     if(error){
+//         console.log('Unable to connect to weather service')
+//     }
 
-    else if(response.body.error){
-        console.log('Unable to find location')
-    }
-    else{
-        console.log(response.body.current)
-    }
+//     else if(response.body.error){
+//         console.log('Unable to find location')
+//     }
+//     else{
+//         console.log(response.body.current)
+//     }
     
-})
+// })
 
 
 //Requesting Current weather data from API using HTTP requests
-request({
-    url: url,
-    json: true
-}, (error, response) =>{
+// request({
+//     url: url,
+//     json: true
+// }, (error, response) =>{
 
-    //Add Error Handling here
-    if(error){
-        console.log('Unable to connect to weather service')
-    }
+//     //Add Error Handling here
+//     if(error){
+//         console.log('Unable to connect to weather service')
+//     }
 
-    else if(response.body.error){
-        console.log('Unable to find location')
-    }
-    else{
-        console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature+" degrees out. It feels like "+ response.body.current.feelslike + " degrees out.")
-    }
+//     else if(response.body.error){
+//         console.log('Unable to find location')
+//     }
+//     else{
+//         console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature+" degrees out. It feels like "+ response.body.current.feelslike + " degrees out.")
+//     }
     
+// })
+
+
+geocode('Philadelphia', (error,data)=>{
+    console.log('Error', error)
+    console.log('Data', data)
 })
