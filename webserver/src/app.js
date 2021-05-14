2b1e1038273b6d3399c4aff385b6cd6b5269282e
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-14 16:20:40 -0700

    Added comments to code blocks

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -8,9 +8,16 @@ const path = require('path')
 
 //Init an express server
 const app = express()
-const publicDirectoryPath=path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath=path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engines and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static ditectory to serve
 app.use(express.static(publicDirectoryPath))
 
 
