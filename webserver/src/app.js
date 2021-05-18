8f7b2868bd4525d4092f5a35d627de4625b5bf5b
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-18 00:27:22 -0700

    Set up app for Heroku

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -11,7 +11,7 @@ const forecast = require('./utils/forecast')
 
 //Init an express server
 const app = express()
-const port = process.env,PORT || 3000
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath=path.join(__dirname, '../public')
