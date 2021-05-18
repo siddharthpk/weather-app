4a39b2325a5ae96e8412153d37b0985bcd1ea883
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-18 00:04:42 -0700

    Added weather output to ui

--- webserver/public/js/fetchapi.js
+++ webserver/public/js/fetchapi.js
@@ -7,6 +7,11 @@ const weatherForm = document.querySelector('form')
 //On adding any input in  the form
 const searchElement = document.querySelector('input')
 
+// For multiple paragraph elements we use id to separate
+const message1 = document.querySelector('#message1')
+const message2 = document.querySelector('#message2')
+
+
 // Event listener with Event 'e'
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -14,15 +19,22 @@ weatherForm.addEventListener('submit', (e) => {
     //Grabbing user input for location
     const location = searchElement.value
 
-    console.log(location)
+    //console.log(location)
+    // Changing paragraph content according to the api response
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
         // Invoking Fetch API
         fetch('http://localhost:3000/weather?address=' + location).then((response) => {
             response.json().then((data)=>{
                 if(data.error){
-                    return console.log(data.error)
+                    message1.textContent= data.error
+                    return message1.textContent
                 }
-                console.log(data.location)
-                console.log(data.forecast)
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+                // console.log(data.location)
+                // console.log(data.forecast)
             })
         })
 })
