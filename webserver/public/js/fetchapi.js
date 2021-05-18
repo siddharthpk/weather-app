706ec7b7c291c33935305ec0e1b8db8f50a51e73
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-05-17 20:48:12 -0700

    Added user input for location & dynamic api calls

--- webserver/public/js/fetchapi.js
+++ webserver/public/js/fetchapi.js
@@ -1,11 +1,28 @@
 console.log('Client side JS file is loaded')
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data)=>{
-        if(data.error){
-            return console.log(data.error)
-        }
-        console.log(data.location)
-        console.log(data.forecast)
-    })
+
+//On Clicking the button in form
+const weatherForm = document.querySelector('form')
+
+//On adding any input in  the form
+const searchElement = document.querySelector('input')
+
+// Event listener with Event 'e'
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    //Grabbing user input for location
+    const location = searchElement.value
+
+    console.log(location)
+        // Invoking Fetch API
+        fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+            response.json().then((data)=>{
+                if(data.error){
+                    return console.log(data.error)
+                }
+                console.log(data.location)
+                console.log(data.forecast)
+            })
+        })
 })
